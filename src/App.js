03c74979e735c70c05ea2997281694f632b23633
Henrique Heiden <henrique.heiden@gmail.com>
2019-04-13 22:14:31 -0300

    Function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { uniqueId } from 'lodash';
+import { filesize } from 'filesize'
 
 import GlobalStyle from './styles/global';
 import { Container, Content } from './styles';
@@ -6,18 +8,47 @@ import Upload from './components/Upload';
 import FileList from './components/FileList';
 
 
+
+
 class App extends Component {
-  render() {
-    return (
-        <Container>
-            <Content>
-            <Upload />
-            <FileList />
-            </Content>
-            <GlobalStyle/>
-        </Container>
-        );
-    }
+
+  state = {
+    uploadedFiles: [],
   }
-  
-  export default App;
+
+  handleUpload = files => {
+    const uploadedFiles = files.map(file => ({
+      file,
+      id: uniqueId(),
+      name: file.name,
+      readableSize: filesize(file.size),
+      prpeview: URL.createObjectURL(file),
+      progress: 0,
+      uploaded: false,
+      error: false,
+      url: null,
+
+    }))
+    this.setState({
+      uploadedFiles: this.state.uploadedFiles.concat(uploadedFiles)
+    });    
+  };
+
+
+  render() {
+
+    const { uploadedFiles } = this.state;
+    
+    return (
+      <Container>
+        <Content>
+          <Upload onUpload={this.handleUpload} />
+          { !!uploadedFiles.length && (<FileList  files={uploadedFiles} />)}
+        </Content>
+        <GlobalStyle />
+      </Container>
+    );
+  }
+}
+
+export default App;
--- src/components/FileList/index.js
+++ src/components/FileList/index.js
@@ -5,41 +5,49 @@ import { MdCheckCircle, MdError, MdLink } from 'react-icons/md';
 
 import { Container, FileInfo, Preview } from './styles';
 
-const FileList = () => (
+const FileList = ({ files }) => (
     <Container>
-        <li>
-            <FileInfo>
-                <Preview src="https://img.elo7.com.br/product/zoom/18038FD/cachorrinho-lhasa-apso-racas.jpg" />
+        {files.map(uploadedFile => (
+            <li>
+                <FileInfo>
+                    <Preview src={uploadedFile.preview} />
+                    <div>
+                        <strong>{uploadedFile.name}</strong>
+                        <span>{uploadedFile.readableSize} <button onclick={() => { }}> Excluir </button>
+                        </span>
+                    </div>
+                </FileInfo>
                 <div>
-                    <strong> profile.png</strong>
-                    <span>64kb <button onclick={() => { }}> Excluir </button></span>
+                   {!uploadedFile.uploaded && !uploadedFile.error && (
+                        <CircularProgressbar
+                        styles={{
+                            root: { width: 24 },
+                            path: { stroke: '#7159c1' }
+                        }}
+                        strokeWidth={10}
+                        percentage={uploadedFile.progress}
+                    />
+                   ) }
+
+                    {!uploadedFile.url && (
+                        <a
+                        href="https://img.elo7.com.br/product/zoom/18038FD/cachorrinho-lhasa-apso-racas.jpg"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+
+                        <MdLink style={{ marginRight: 8 }} size={24} color="#222" />
+                    </a>
+                    )}
+
+                    { uploadedFile.uploaded &&  <MdCheckCircle size={24} color="#78e5d5" />}
+                    { uploadedFile.error &&   <MdError size={24} color="#e57878" />}
+                   
+
                 </div>
-            </FileInfo>
-            <div>
-                <CircularProgressbar
-                    styles={{
-                        root: { width: 24 },
-                        path: { stroke: '#7159c1' }
-                    }}
-                    strokeWidth={10}
-                    percentage={60}
-                />
 
-                <a
-                    href="https://img.elo7.com.br/product/zoom/18038FD/cachorrinho-lhasa-apso-racas.jpg"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-
-                    <MdLink style={{ marginRight: 8 }} size={24} color="#222" />
-                </a>
-
-                <MdCheckCircle size={24} color="#78e5d5" />
-                <MdError size={24} color="#e57878" />
-
-            </div>
-
-        </li>
+            </li>
+        ))}
     </Container>
 );
 
